cc5f6268a6afe7446a189ac9706f1c87caf79887
Damianmalo <Damian.malo12@gmail.com>
2024-11-07 11:52:36 -0500

    Add files via upload

---
+++ Password.py
@@ -0,0 +1,31 @@
+import re
+
+def check_password_strength(password):
+    if len(password) < 12:
+        return "Weak: Password must beat least 8 characters long"
+    
+    
+    
+    
+    if not re.search(r'[A-Z]', password):
+        return "Weak: Password must contain at least 1 uppercase letter" 
+    
+    
+    if not re.search(r'[a-z]', password):
+        return "Weak: Password must contain at least 1 lowercase letter"
+    
+    
+    if not re.search(r'[0-9]', password):
+        return "Weak: Password must contain at least 1 number"
+    
+    
+    if not re.search(r'[!@#$%^&*()_,.?":{}|<>]', password):
+        return "Weak: Password must contain $ in it."
+    return "Strong: Password is strong!"
+while True:
+    password = input("Enter a password to check its strength: ")
+    result = check_password_strength(password)
+    print(result)
+    if result == "Strong: Password is strong!":
+        break 
+        
